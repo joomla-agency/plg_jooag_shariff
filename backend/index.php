c839704f0e32edf1b5f16e35738a7a0d5651da6b
Ufuk Avcu <ufuk@jooag.de>
2019-04-03 13:51:28 +0200

    Fix

--- backend/index.php
+++ backend/index.php
@@ -1,52 +1,27 @@
 <?php
-
 require_once __DIR__.'/vendor/autoload.php';
-
 use Heise\Shariff\Backend;
-
 /**
- * Demo Application using Shariff Backend
- */
+* Demo Application using Shariff Backend
+*/
 class Application
 {
-    /**
-     * Sample configuration
-     *
-     * @var array
-     */
-    private static $configuration = [
-        'cache' => [
-            'ttl' => 60
-        ],
-        'domains' => [
-            'www.heise.de',
-            'www.ct.de'
-        ],
-        'services' => [
-            'Facebook',
-            'LinkedIn',
-            'Reddit',
-            'StumbleUpon',
-            'Flattr',
-            'Pinterest',
-            'Xing',
-            'AddThis',
-            'Vk'
-        ]
-    ];
-
-    public static function run()
-    {
-        header('Content-type: application/json');
-
-        $url = isset($_GET['url']) ? $_GET['url'] : '';
-        if ($url) {
-            $shariff = new Backend(self::$configuration);
-            echo json_encode($shariff->get($url));
-        } else {
-            echo json_encode(null);
-        }
-    }
+	public static function run()
+	{
+		$file = file_get_contents(__DIR__ . '/shariff.json');
+		$configuration = json_decode($file, true);
+		header('Content-type: application/json');
+		$url = isset($_GET['url']) ? $_GET['url'] : '';
+		
+		if($url)
+		{
+			$shariff = new Backend($configuration);
+			echo json_encode($shariff->get($url));
+		}
+		else
+		{
+			echo json_encode(null);
+		}
+	}
 }
-
 Application::run();
