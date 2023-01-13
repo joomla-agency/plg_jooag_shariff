89a47285e4ceb53dcfe3b08a9685a5a56c9b0264
Ufuk Avcu <ufuk@jooag.de>
2023-01-13 13:34:37 +0100

    Fix Language Code Detection Warning

--- jooag_shariff.php
+++ jooag_shariff.php
@@ -112,7 +112,7 @@ class plgSystemJooag_Shariff extends JPlugin
 		{
 			$configs = explode(' ', $matches[1][0]);
 			$config = array();
-
+			
 			foreach ($configs as $item)
 			{
 				list($key, $value) = explode("=", $item);
@@ -245,7 +245,7 @@ class plgSystemJooag_Shariff extends JPlugin
 
 		$html  = '<div class="shariff"';
 		$html .= ($this->params->get('shariff_counter')) ? ' data-backend-url="/plugins/system/jooag_shariff/backend/"' : '';
-		$html .= ' data-lang="' . JLanguageHelper::getLanguages('lang_code')[ JFactory::getLanguage()->getTag() ]->sef . '"';
+		$html .= ' data-lang="' . JLanguageHelper::getLanguages()['0']->sef . '"';
 		$html .= (array_key_exists('orientation', $config)) ? ' data-orientation="'.$config['orientation'] . '"' : ' data-orientation="'.$this->params->get('data_orientation') . '"';
 		$html .= (array_key_exists('theme', $config)) ? ' data-theme="'.$config['theme'].'"' : ' data-theme="' . $this->params->get('data_theme') . '"';
 		$html .= (array_key_exists('style', $config)) ? ' data-button-style="'.$config['style'].'"' : ' data-button-style="' . $this->params->get('data_style') . '"';
